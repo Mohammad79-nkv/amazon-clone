05acf850adde53710ab49c18c19d9fe027f1ecd7
mohammad <mohammadnv79@gmail.com>
2021-09-18 13:06:47 +0430

    header added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBkIOnCrZKwMQckLCKHPEbskygkWsRLOD8",
+    authDomain: "project-ab776.firebaseapp.com",
+    databaseURL: "https://project-ab776-default-rtdb.firebaseio.com",
+    projectId: "project-ab776",
+    storageBucket: "project-ab776.appspot.com",
+    messagingSenderId: "421666997578",
+    appId: "1:421666997578:web:528aa4ac633aac42586db3",
+    measurementId: "G-5YBYS2LPVG"
+  };
