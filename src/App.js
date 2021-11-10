9da0f71c91901b2abf3067aee204f677b3f1f125
mohammad <mohammadnv79@gmail.com>
2021-11-10 09:57:31 +0330

    footer added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Checkout from "./component/Checkout";
+import Footer from "./component/footer/Footer";
 import Header from "./component/Header";
 import Home from "./component/Home";
 import Login from "./component/login/Login";
@@ -32,26 +33,29 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <Header />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <ToastContainer />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/checkout" exact component={Checkout} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/register" exact component={Register} />
-        <Route path="/payment" exact component={Payment} />
-      </Switch>
+      <div className="main">
+        <Header />
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <ToastContainer />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/checkout" exact component={Checkout} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/payment" exact component={Payment} />
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/footer/Footer.jsx
@@ -0,0 +1,62 @@
+import './footer.css'
+const Footer = () => {
+    return ( 
+        <footer>
+            <div className="footer__container">
+                <div className="footer__section">
+                    <h4 className="footer__title">Get to Know Us</h4>
+                    <div className="footer__items">
+                        <a href="#" className="footer__link">Careers</a>
+                        <a href="#" className="footer__link">Blog</a>
+                        <a href="#" className="footer__link">About Amazon</a>
+                        <a href="#" className="footer__link">Investor Relations</a>
+                        <a href="#" className="footer__link">Amazon Devices
+                        </a>
+                    </div>
+                </div>
+                <div className="footer__section">
+                    <h4 className="footer__title">Make Money with Us</h4>
+                    <div className="footer__items">
+                    <a href="#" className="footer__link">Sell products on Amazon</a>
+                    <a href="#" className="footer__link">Sell on Amazon Business</a>
+                    <a href="#" className="footer__link">Sell apps on Amazon</a>
+                    <a href="#" className="footer__link">Become an Affiliate</a>
+                    <a href="#" className="footer__link">Advertise Your Products</a>
+                    <a href="#" className="footer__link">Self-Publish with Us</a>
+                    <a href="#" className="footer__link">Host an Amazon Hub</a>
+                    <a href="#" className="footer__link">›See More Make Money with Us</a>
+
+                    </div>
+                </div>
+                <div className="footer__section">
+                    <h4 className="footer__title">Amazon Payment Products</h4>
+                    <div className="footer__items">
+                    
+                    <a href="#" className="footer__link">Amazon Business Card</a>
+                    <a href="#" className="footer__link">Shop with Points</a>
+                    <a href="#" className="footer__link">Reload Your Balance</a>
+                    <a href="#" className="footer__link">Amazon Currency Converter</a>
+
+                    </div>
+                </div>
+                <div className="footer__section">
+                    <h4 className="footer__title">Let Us Help You</h4>
+                    <div className="footer__items">
+                    <a href="#" className="footer__link">Amazon and COVID-19</a>
+                    <a href="#" className="footer__link">Your Account</a>
+
+                    <a href="#" className="footer__link">Your Orders</a>
+                    <a href="#" className="footer__link">Shipping Rates & Policies</a>
+                    <a href="#" className="footer__link">Returns & Replacements</a>
+                    <a href="#" className="footer__link">Manage Your Content and Devices</a>
+                    <a href="#" className="footer__link">Amazon Assistant</a>
+                    <a href="#" className="footer__link">Help</a>
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+     );
+}
+ 
+export default Footer;
